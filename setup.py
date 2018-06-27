fbdea57f0438d01b22403938d9333dd6141c258b
defunct <defunct@defunct.io>
2018-06-27 18:42:34 +0000

    bump minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyevmasm',
-    version='0.0.2',
+    version='0.1.0',
     description='Ethereum Virtual Machine (EVM) assembler and disassembler',
     scripts=['evmasm'],
     author='Trail of Bits',
