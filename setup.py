b6b5df86736b62a1c9dada7bdc54be218e722691
Jay Little <computerality@users.noreply.github.com>
2018-12-07 12:38:05 -0500

    Update setup.py version for 0.1.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyevmasm',
-    version='0.1.1',
+    version='0.1.2',
     description='Ethereum Virtual Machine (EVM) assembler and disassembler',
     author='Trail of Bits',
     author_email='evmasm@trailofbits.com',
