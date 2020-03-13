ce154d9820ad1c2dcedcb74b6daa375970fc33e8
computerality <jay@computerality.com>
2020-03-12 21:53:59 -0400

    Update version number for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyevmasm',
-    version='0.2.1',
+    version='0.2.2',
     description='Ethereum Virtual Machine (EVM) assembler and disassembler',
     author='Trail of Bits',
     author_email='evmasm@trailofbits.com',
