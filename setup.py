69c41e4a824d83708c38cb41424a32b980132178
Emilio López <emilio.lopez@trailofbits.com>
2025-08-19 12:41:38 -0300

    pyevmasm: lint with `black`

--- setup.py
+++ setup.py
@@ -1,28 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='pyevmasm',
-    version='0.2.4',
-    description='Ethereum Virtual Machine (EVM) assembler and disassembler',
-    author='Trail of Bits',
-    author_email='evmasm@trailofbits.com',
-    url='https://github.com/trailofbits/pyevmasm',
-    license='Apache License 2.0',
-    packages=['pyevmasm'],
-    python_requires='>=3.8',
-    install_requires=[
-        'future'
-    ],
-    extras_require={
-        'dev': [
-            'nose',
-            'coverage',
-            'flake8'
-        ]
-    },
-    entry_points={
-        'console_scripts': [
-            'evmasm = pyevmasm.__main__:main'
-        ]
-    }
+    name="pyevmasm",
+    version="0.2.4",
+    description="Ethereum Virtual Machine (EVM) assembler and disassembler",
+    author="Trail of Bits",
+    author_email="evmasm@trailofbits.com",
+    url="https://github.com/trailofbits/pyevmasm",
+    license="Apache License 2.0",
+    packages=["pyevmasm"],
+    python_requires=">=3.8",
+    install_requires=["future"],
+    extras_require={"dev": ["nose", "coverage", "flake8"]},
+    entry_points={"console_scripts": ["evmasm = pyevmasm.__main__:main"]},
 )
